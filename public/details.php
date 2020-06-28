dcbdc25021a3458f51bf080f4b95efd543e99de0
Ryu-Nis <west.japan.24sd8@gmail.com>
2020-06-28 20:37:57 +0900

    add data from historical table

--- public/details.php
+++ public/details.php
@@ -21,10 +21,36 @@
             printf("<p>商品コード:  ".$row[1]."</p>") ;
             printf("<p>更新日:  ".$row[2]."</p>") ;
             printf("<p>名前:  ".$row[3]."</p>") ;
-            printf("<p>値段:  ".$row[4]."</p>") ;
-            printf("<p>前日比:  ".$row[5]."％</p>") ;     
+            printf("<p>値段:  ".$row[4]."円</p>") ;
+            printf("<p>前回比:  ".$row[5]."％</p>") ;     
         }
     }
     ?>
+    
+    <h2>値段履歴</h2>
+    
+    <?php
+    $sql = "SELECT date,price from historical_products WHERE code = (SELECT code from products WHERE id=".$id.")order by date desc";
+
+    $res = mysqli_query($link, $sql);
+    $rows = mysqli_num_rows($res);
+
+
+    if($rows){  
+        while($row = mysqli_fetch_array($res)) {
+          // 　$tempHtml .= "";
+            $tempHtml .= "<tr><td>".$row[0]."</td>";
+            $tempHtml .= "<td>".$row[1]."円</td>";
+            $tempHtml .= "</tr>\r\n";  
+        }
+    }
+
+    ?>
+
+    <table border =1>
+      <tr bgcolor="##ccffcc"><td width = "200" >日時</td><td width='200'>値段</td>
+      <?= $tempHtml ?></tr>
+    </table>
+
   </body>
 </html>
